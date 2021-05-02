da78d637afcbd04bd3c98b0435b3507633c71cd9
StraightCat <42774281+NavinShrinivas@users.noreply.github.com>
2021-05-02 10:20:08 +0530

    changing api and json to process 1 whole week
    This gives a lot more hits and am positive will find more centers

--- main.py
+++ main.py
@@ -12,36 +12,36 @@ def task():
     tot=1
     hits=0
     hitlist=[]
-    while rdln:
+   while rdln:
         date = str(datetime.date.today())
-        link="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode="+rdln.strip()+"&date="+date[8]+date[9]+"-"+date[5]+date[6]+"-"+"2021
+        link="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode="+rdln.strip()+"&date="+date[8]+date[9]+"-"+date[5]+date[6]+"-"+"2021"
         x = requests.get(link)
         # print(link)
         data = json.loads(x.text)
-        f_list=data["sessions"]
+        f_list=data["centers"]
         count=1
         for i in f_list:
-            if(i['min_age_limit']==18):
-                hits+=1
-                print(tot,".",count,":")
-                print("\tname of hospital :",i['name'])
-                hitlist.append(i['name'])
-                print("\tpincode of hospital :",i['pincode'])
-                print("\tAvailable doses :",i['available_capacity'])
-                print("\tAge limit :",i['min_age_limit'])
+            for j in i["sessions"]:
+                if(j['min_age_limit']==18 && j['available_capacity']>0):
+                    hits+=1
+                    print(tot,".",count,":")
+                    print("\tname of hospital :",i['name'])
+                    if i['name'] not in hitlist:
+                        hitlist.append(i['name'])
+                    print("\tpincode of hospital :",i['pincode'])
+                    print("\tAvailable doses :",j['available_capacity'])
+                    print("\tAge limit :",j['min_age_limit'])
+                    print("\tOn date :",j["date"])
             count+=1
         tot+=1
         rdln=f.readline()
+    print(hitlist)
 
     if(hits==0):
         notify.send('Nope! ran through db and no centers found for <45')
         notify.send('Nope! ran through db and no centers found for <45')
-    elif(hits==1):
-        strin="Yes! ran through db and found one. schedule it in "+str(hitlist[0])
-        notify.send(strin)
-        notify.send(strin)
     else:
-        strin="Yes! multiple centers found when ran through db first being"+str(hitlist[0])
+        strin="Yes! centers found when ran through db first being : "+str(hitlist[0])
         notify.send(strin)
         notify.send(strin)
 
