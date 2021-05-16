ce9f0287192954afa9c09c412cfbe708b98d112c
Navin Shrinivas <karupal2002@gmail.com>
2021-05-16 20:54:03 +0530

    huh

--- main2.py
+++ main2.py
@@ -3,6 +3,8 @@ import json
 import datetime
 # from notify_run import Notify
 from time import sleep
+from fake_useragent import UserAgent
+import http.client
 # s
 
 # notify = Notify()
@@ -19,23 +21,37 @@ def task():
         try:
             date = str(datetime.date.today())
             link="https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode="+rdln.strip()+"&date="+date[8]+date[9]+"-"+date[5]+date[6]+"-"+"2021"
-            x = requests.get(link)
-            # print(x.text)
-            data = json.loads(x.text)
+            mod="/api/v2/appointment/sessions/public/calendarByPin?pincode="+rdln.strip()+"&date="+date[8]+date[9]+"-"+date[5]+date[6]+"-"+"2021"
+            temp_user_agent = UserAgent()
+            header = {'User-Agent': temp_user_agent.random}
+            conn = http.client.HTTPSConnection("cdn-api.co-vin.in")
+            payload = ''
+            headers = {'User-Agent': temp_user_agent.random}
+            conn.request("GET", mod, payload, headers)
+            res = conn.getresponse()
+            data = res.read()
+            x=data.decode("utf-8")
+            sleep(4)
+            if type(x)!=dict:
+                print("oops")
+                # print(x)
+            data = json.loads(x)
             f_list=data["centers"]
             count=1
             for i in f_list:
                 for j in i["sessions"]:
                     if(j['min_age_limit']==18 and j['available_capacity']>0):
-                        hits+=1
-                        print(tot,".",count,":")
-                        print("\tname of hospital :",i['name'])
-                        if i['name'] not in hitlist:
-                            hitlist.append(i['name'])
-                        print("\tpincode of hospital :",i['pincode'])
-                        print("\tAvailable doses :",j['available_capacity'])
-                        print("\tAge limit :",j['min_age_limit'])
-                        print("\tOn date :",j["date"])
+                        if(j["vaccine"]=="COVAXIN"):
+                            hits+=1
+                            print(tot,".",count,":")
+                            print("\tname of hospital :",i['name'])
+                            if i['name'] not in hitlist:
+                                hitlist.append(i['name'])
+                            print("\tpincode of hospital :",i['pincode'])
+                            print("\tAvailable doses :",j['available_capacity'])
+                            print("\tAge limit :",j['min_age_limit'])
+                            print("\tOn date :",j["date"])
+                            print("\tVaccine :",j["vaccine"])
                         # phone_obj=open("jsonformat.json")
                         # y=requests.post('https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP',data=phone_obj)
                         # print(y.text)
